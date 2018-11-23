604abe8cb706f55742e5fc8fe0e695a4eb4fc026
svd2802 <narutoche@bk.ru>
2018-11-23 21:31:06 +0600

    23.11 v2

--- src/Components/InputForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const InputForm = (props) => (
-    <div className="inputForm">
-        <input onChange={props.onInputChange} value={props.newItem}/>
-        <button onClick={props.onAddButtonClick}>Add</button>
-        <button onClick={props.onClearButtonClick}>Clear</button>
-    </div>
-);
